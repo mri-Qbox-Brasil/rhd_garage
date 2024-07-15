27087b77d8cbd69b75334f1a59702121bc818eb1
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-07-15 18:37:21 -0300

    Update qb.sql

--- sql/qb.sql
+++ sql/qb.sql
@@ -27,6 +27,26 @@ CREATE TABLE IF NOT EXISTS `player_vehicles` (
   KEY `license` (`license`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+        AND COLUMN_NAME='balance' AND TABLE_NAME='player_vehicles') ) THEN
+    ALTER TABLE player_vehicles ADD balance int(11) NOT NULL DEFAULT 0;
+END IF;
+
+IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+        AND COLUMN_NAME='paymentamount' AND TABLE_NAME='player_vehicles') ) THEN
+    ALTER TABLE player_vehicles ADD paymentamount int(11) NOT NULL DEFAULT 0;
+END IF;
+
+IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+        AND COLUMN_NAME='paymentsleft' AND TABLE_NAME='player_vehicles') ) THEN
+    ALTER TABLE player_vehicles ADD paymentsleft int(11) NOT NULL DEFAULT 0;
+END IF;
+
+IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+        AND COLUMN_NAME='financetime' AND TABLE_NAME='player_vehicles') ) THEN
+    ALTER TABLE player_vehicles ADD financetime int(11) NOT NULL DEFAULT 0;
+END IF
+
 CREATE TRIGGER rhd_garage_update_impound_plate
 AFTER UPDATE ON player_vehicles
 FOR EACH ROW
