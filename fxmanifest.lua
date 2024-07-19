e44a9261fc77ecacd2a41e296ca71d8dec9ca555
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-07-19 15:07:07 -0300

    [FEAT] Separando a atualização do arquivo main
    tive que mexer no fxmanifest :cry:

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,6 +18,7 @@ client_scripts {
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
+    'server/db_update.lua',
     'server/main.lua',
     'server/police_impound.lua',
     'server/storage.lua',
